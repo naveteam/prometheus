c6c3032c327e968e598348e486f73d3cf4cbf416
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-07-24 21:06:52 -0300

    fix: remove some packages

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,3 @@
 export { default as useClickOutside } from './useClickOutside'
 export { default as useDebounce } from './useDebounce'
 export { default as useHotKey } from './useHotKey'
-export { default as useWindowSize } from './useWindowSize'
-export { default as useDevice } from './useDevice'
