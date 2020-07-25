5ae781a8754110fa93d293c0b17a2bafce2bdc94
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-07-24 21:20:08 -0300

    feat(hook): add useWindowSize hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useClickOutside } from './useClickOutside'
 export { default as useDebounce } from './useDebounce'
 export { default as useHotKey } from './useHotKey'
+export { default as useWindowSize } from './useWindowSize'
